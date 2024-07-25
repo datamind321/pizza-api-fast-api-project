0b596094b44117099f7c72dcbdd1776b6a83cc89
Data-Mind 2.0 <112772762+datamind321@users.noreply.github.com>
2024-07-25 17:02:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-engine = create_engine("postgresql://pizza_g6pm_user:klhvCHdLv22SXpKphttLFSQXaqwk6lno@5432/pizza_g6pm",connect_args={"check_same_thread": False})
+engine = create_engine("postgresql://pizza_g6pm_user:klhvCHdLv22SXpKphttLFSQXaqwk6lno@dpg-cqh31k2ju9rs73ef9okg-a/pizza_g6pm",connect_args={"check_same_thread": False})
 
 Base = declarative_base()
 
