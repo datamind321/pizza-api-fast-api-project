2cc8ad11aaaf0c80406f7758d6b52ba1ed4feff9
Data-Mind 2.0 <112772762+datamind321@users.noreply.github.com>
2024-07-25 17:16:17 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ from sqlalchemy_utils.types import ChoiceType
 class User(Base):
     __tablename__='user'
     id = Column(Integer,primary_key=True)
-    username=Column(String(8),unique=True)
-    email = Column(String(50),unique=True)
+    username=Column(String,unique=True)
+    email = Column(String,unique=True)
     password = Column(Text,nullable=True)
     is_staff = Column(Boolean,default=False)
     is_active = Column(Boolean,default=False)
